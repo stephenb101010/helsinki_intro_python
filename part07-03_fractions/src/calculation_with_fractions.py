6438de56998e122730c38a0c23340a713934867a
Stephen B <stephenb.k101010@null.net>
2022-01-14 18:22:39 -0700

    massive update

--- part07-03_fractions/src/calculation_with_fractions.py
+++ part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,7 @@
-# Write your solution here
+from fractions import Fraction
+
+def fractionate(amount: int):
+    fractions = []
+    for i in range(amount):
+        fractions.append(Fraction(1, amount))
+    return fractions
